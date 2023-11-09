f1fe656e8efffb91691f12f366fce784009368df
Cely Febita <cely.febita21@students.unila.ac.id>
2023-11-09 08:13:22 +0700

    Commit perubahan page login

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,10 +5,9 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card border-0 shadow rounded-3 my-5">
+       
           <div class="card-body p-4 p-sm-5">
-            <i class="fa-solid fa-graduation-cap"></i>
-            <h5 class="card-title text-center mb-5 fw-light fs-5">BesWan Login</h5>
+            <h5 class="text-center text-bold">BesWan Login</h5><br>
             <form action="/checkLogin" method="post">
               <div class="form-floating mb-3">
                 <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
@@ -20,7 +19,7 @@
                 <label for="floatingPassword">Kata Sandi</label>
               </div>
 
-              <a href="#forgot">Lupa Kata Sandi?</a>
+
               <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
                 <label class="form-check-label" for="rememberPasswordCheck">
@@ -30,17 +29,24 @@
               <div class="d-grid">
                 <button class="btn btn-login btn-success text-uppercase fw-bold" type="submit">Masuk</button>
               </div>
+
+              <br><div class="text-center f">
+              <a href="#forgot">Lupa Kata Sandi?</a>
+              </div>
               <!-- Register buttons -->
-              <br><br>
-              <div class="text-center">
-                <p>Tidak punya akun? <a href="#!">Buat Akun</a></p>
+              
+              <br><br><div class="text-center">
+              <div class="underline"></div>
+               <br> <p>Tidak punya akun? <a href="#!">Buat Akun</a></p>
 
               </div>
+              <br><br>
             </form>
           </div>
         </div>
+        </div>
       </div>
     </div>
-  </div>
+    
 </body>
 <?= $this->endsection() ?>
