7eb143108762716d6e511bc6358af5c82d830fd9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-18 17:26:20 +0700

    don

--- app/Controllers/AkunController.php
+++ app/Controllers/AkunController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\UserModel;
+use App\Models\ProgramModel;
+use App\Models\DonaturModel;
 
 class AkunController extends BaseController
 {
@@ -19,12 +21,22 @@ class AkunController extends BaseController
 
     public function index()
     {
+        $userModel = new UserModel();
+        $programModel = new ProgramModel();
+        $donaturModel = new DonaturModel();
+
         $this->builder->select('id, username, email');
         $query = $this->builder->get();
+        $akun = count($userModel->findAll());
+        $program = count($programModel->findAll());
+        $donatur = count($donaturModel->findAll());
         
         $data = [
             'title' => 'Dashboard Admin',
             'akun'   => $query->getResult(),
+            'akuns'  => $akun,
+            'program' => $program,
+            'donatur' => $donatur
         ];
 
         return view('admin/admin_dashboard', $data);
--- app/Views/admin/admin_dashboard.php
+++ app/Views/admin/admin_dashboard.php
@@ -163,7 +163,7 @@
 									<div class="row no-gutters align-items-center">
 										<div class="col mr-2">
 											<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Akun</div>
-											<div class="h5 mb-0 font-weight-bold text-gray-800">#</div>
+											<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $akuns?></div>
 										</div>
 										<div class="col-auto">
 											<i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -180,7 +180,7 @@
 									<div class="row no-gutters align-items-center">
 										<div class="col mr-2">
 											<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Program</div>
-											<div class="h5 mb-0 font-weight-bold text-gray-800">#</div>
+											<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $program ?></div>
 										</div>
 										<div class="col-auto">
 											<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -199,7 +199,7 @@
 											<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Donatur</div>
 											<div class="row no-gutters align-items-center">
 												<div class="col-auto">
-													<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">#</div>
+													<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $donatur?></div>
 												</div>
 											</div>
 										</div>
